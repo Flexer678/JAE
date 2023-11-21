9160382dbba6766aa3b7710ba7a831eea54896da
Flexer678 <81823862+Flexer678@users.noreply.github.com>
2023-11-21 11:09:01 -0600

    navbar added

--- jae/src/main/java/Pages/SearchPage.java
+++ jae/src/main/java/Pages/SearchPage.java
@@ -3,9 +3,11 @@ package Pages;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 
-public class SearchPage implements Initializable {
+public class SearchPage implements Initializable,  EventHandler<ActionEvent> {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
---
+++ jae/src/main/java/widgets/navbarController.java
@@ -0,0 +1,13 @@
+package widgets;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class navbarController extends Initilizable, EventHandler<ActionEvent>{ {
+       @FXML
+    private Label username;
+
+    @FXML
+    Label username_profile;
+
+}
