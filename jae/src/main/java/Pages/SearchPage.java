822c20b54baaa479081833447f8d97d113f1f663
jerzyav <jerzyvillafuerte@yahoo.com>
2023-11-21 11:30:50 -0600

    updated item.fxml

--- jae/src/main/java/Pages/SearchPage.java
+++ jae/src/main/java/Pages/SearchPage.java
@@ -1,18 +1,18 @@
-package Pages;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.Initializable;
-
-public class SearchPage implements Initializable,  EventHandler<ActionEvent> {
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
-    }
-    
-}
+//package Pages;
+//
+//import java.net.URL;
+//import java.util.ResourceBundle;
+//
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.fxml.Initializable;
+//
+//public class SearchPage implements Initializable,  EventHandler<ActionEvent> {
+//
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+//    }
+//
+//}
--- jae/src/main/java/widgets/navbarController.java
+++ jae/src/main/java/widgets/navbarController.java
@@ -1,13 +1,15 @@
-package widgets;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class navbarController extends Initilizable, EventHandler<ActionEvent>{ {
-       @FXML
-    private Label username;
-
-    @FXML
-    Label username_profile;
-
-}
+//package widgets;
+//
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Label;
+//
+//public class navbarController extends EventHandler<ActionEvent> {
+//    @FXML
+//    private Label username;
+//
+//    @FXML
+//    Label username_profile;
+//
+//}
