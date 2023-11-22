79ab45937dc7ce7578b572dc60215548f9e0774a
jerzyav <jerzyvillafuerte@yahoo.com>
2023-11-22 09:44:39 -0600

    initialized navholder on detailpage

--- jae/src/main/java/Pages/DetailPage.java
+++ jae/src/main/java/Pages/DetailPage.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import assets.localFiles;
 import datamodel.CartItem_model;
@@ -9,6 +11,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,7 +26,7 @@ import javafx.stage.Stage;
 import server.BookApi;
 import widgets.navbarController;
 
-public class DetailPage  {
+public class DetailPage implements Initializable {
     @FXML
     Text name, description; 
     
@@ -83,5 +86,14 @@ public class DetailPage  {
             AnchorPane box =loader.load();
             navbarController controller = loader.getController();
             navholder.getChildren().add(box);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        try {
+            display_navholder();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
